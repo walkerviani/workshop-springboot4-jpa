1d14407a961d059e7011329441544b1e8f98536f
Walker Viani <walkeryslan@gmail.com>
2025-12-17 19:29:08 -0300

    camada de serviço, registro de componentes

---
+++ src/main/java/com/walkerviani/primeirospring/services/UserService.java
@@ -0,0 +1,26 @@
+package com.walkerviani.primeirospring.services;
+
+import com.walkerviani.primeirospring.entities.User;
+import com.walkerviani.primeirospring.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class UserService {
+
+    @Autowired
+    private UserRepository repository;
+
+    public List<User> findAll(){
+        return repository.findAll();
+    }
+
+    public User findById(Long id){
+        Optional<User> user = repository.findById(id);
+        return user.get();
+    }
+}
