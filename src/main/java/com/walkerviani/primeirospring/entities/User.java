29947838d0f9f249dba7f01bb83a0a05dca0d1de
Walker Viani <walkeryslan@gmail.com>
2025-12-17 18:33:09 -0300

    banco de dados H2, test profile e JPA

--- src/main/java/com/walkerviani/primeirospring/entities/User.java
+++ src/main/java/com/walkerviani/primeirospring/entities/User.java
@@ -1,10 +1,17 @@
 package com.walkerviani.primeirospring.entities;
 
+import jakarta.persistence.*;
+
 import java.io.Serial;
 import java.io.Serializable;
 
+@Entity
+@Table(name= "tb_user")
 public class User implements Serializable {
+
     private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
     private String email;
