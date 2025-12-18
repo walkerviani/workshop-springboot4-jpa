da9ab93baa92cf96e3b11827521fd20e1bc17f80
Walker Viani <walkeryslan@gmail.com>
2025-12-18 17:53:34 -0300

    entidade Category

---
+++ src/main/java/com/walkerviani/primeirospring/entities/Category.java
@@ -0,0 +1,53 @@
+package com.walkerviani.primeirospring.entities;
+
+import jakarta.persistence.*;
+
+import java.io.Serializable;
+
+@Entity
+@Table(name="tb_category")
+public class Category implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy= GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+
+    public Category(){
+
+    }
+    public Category(Long id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Category category = (Category) o;
+        return id.equals(category.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return id.hashCode();
+    }
+}
---
+++ src/main/java/com/walkerviani/primeirospring/services/CategoryService.java
@@ -0,0 +1,25 @@
+package com.walkerviani.primeirospring.services;
+
+import com.walkerviani.primeirospring.entities.Category;
+import com.walkerviani.primeirospring.repositories.CategoryRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class CategoryService {
+
+    @Autowired
+    private CategoryRepository repository;
+
+    public List<Category> findAll(){
+        return repository.findAll();
+    }
+
+    public Category findById(Long id){
+        Optional<Category> Category = repository.findById(id);
+        return Category.get();
+    }
+}
