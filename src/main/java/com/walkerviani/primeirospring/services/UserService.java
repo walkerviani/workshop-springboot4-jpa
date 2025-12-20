930acf8a1b07084211a279accd5fa4bac8272567
Walker Viani <walkeryslan@gmail.com>
2025-12-20 19:07:36 -0300

    atualização de User

--- src/main/java/com/walkerviani/primeirospring/services/UserService.java
+++ src/main/java/com/walkerviani/primeirospring/services/UserService.java
@@ -15,20 +15,34 @@ public class UserService {
     @Autowired
     private UserRepository repository;
 
-    public List<User> findAll(){
+    public List<User> findAll() {
         return repository.findAll();
     }
 
-    public User findById(Long id){
+    public User findById(Long id) {
         Optional<User> user = repository.findById(id);
         return user.get();
     }
 
-    public User insert(User obj){
-       return repository.save(obj);
+    public User insert(User obj) {
+        return repository.save(obj);
     }
 
-    public void delete(Long id){
+    public void delete(Long id) {
         repository.deleteById(id);
     }
+
+    public User update(Long id, User obj) {
+        User entity = repository.getReferenceById(id);
+        updateData(entity, obj);
+        return repository.save(entity);
+    }
+
+    private void updateData(User entity, User obj) {
+        entity.setName(obj.getName());
+        entity.setEmail(obj.getEmail());
+        entity.setPhone(obj.getPhone());
+    }
+
+
 }
