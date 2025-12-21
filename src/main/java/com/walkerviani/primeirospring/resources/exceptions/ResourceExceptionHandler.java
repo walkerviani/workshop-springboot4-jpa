9fe6650bf4198ed5d4dd056cd75809465af2180a
Walker Viani <walkeryslan@gmail.com>
2025-12-21 15:54:14 -0300

    tratamento de exceção - delete

--- src/main/java/com/walkerviani/primeirospring/resources/exceptions/ResourceExceptionHandler.java
+++ src/main/java/com/walkerviani/primeirospring/resources/exceptions/ResourceExceptionHandler.java
@@ -1,5 +1,6 @@
 package com.walkerviani.primeirospring.resources.exceptions;
 
+import com.walkerviani.primeirospring.services.exceptions.DatabaseException;
 import com.walkerviani.primeirospring.services.exceptions.ResourceNotFoundException;
 import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.http.HttpStatus;
@@ -16,6 +17,15 @@ public class ResourceExceptionHandler {
     public ResponseEntity<StandardError> resourceNotFound(ResourceNotFoundException e, HttpServletRequest request) {
         String error = "Resource not found";
         HttpStatus status = HttpStatus.NOT_FOUND;
+        StandardError errorResponse = new StandardError(Instant.now(), status.value(), error, e.getMessage(), request.getRequestURI());
+        return ResponseEntity.status(status).body(errorResponse);
+
+    }
+
+    @ExceptionHandler(DatabaseException.class)
+    public ResponseEntity<StandardError> database(DatabaseException e, HttpServletRequest request) {
+        String error = "Database error";
+        HttpStatus status = HttpStatus.BAD_REQUEST;
         StandardError errorResponse = new StandardError(Instant.now(), status.value(), error, e.getMessage(), request.getRequestURI());
         return ResponseEntity.status(status).body(errorResponse);
 
--- src/main/java/com/walkerviani/primeirospring/services/UserService.java
+++ src/main/java/com/walkerviani/primeirospring/services/UserService.java
@@ -2,9 +2,11 @@ package com.walkerviani.primeirospring.services;
 
 import com.walkerviani.primeirospring.entities.User;
 import com.walkerviani.primeirospring.repositories.UserRepository;
+import com.walkerviani.primeirospring.services.exceptions.DatabaseException;
 import com.walkerviani.primeirospring.services.exceptions.ResourceNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Component;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -30,7 +32,13 @@ public class UserService {
     }
 
     public void delete(Long id) {
-        repository.deleteById(id);
+        try {
+            repository.deleteById(id);
+        } catch (EmptyResultDataAccessException e) {
+            throw new ResourceNotFoundException(id);
+        } catch (DataIntegrityViolationException e) {
+            throw new DatabaseException(e.getMessage());
+        }
     }
 
     public User update(Long id, User obj) {
